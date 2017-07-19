45f0f4290d85022c97f261ae95689e89c2d20eb5
morsko1 <morsko1@yandex.ru>
2017-07-20 00:26:53 +0300

    added games list to Team component

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import * as Constants from '../../constants/';
 
 class Team extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+  }
+
+  saveLinkPropsToState (params) {
+    const season = params.season;
+    const country = params.country;
+    const league = params.league;
+    const team = params.team;
+    this.setState({
+      season: season,
+      country: country,
+      league: league,
+      team: team
+    });
+  }
+
+  ajaxCallGames (season, country, league, team) {
+    let gameNum = 0;
+    let gamesList= [];
+    const URL = `${Constants.BASE_URL}${season}/${country}/${league}/games.json`;
+    axios.get(URL)
+    .then ((res) => {
+      res.data.games.forEach((item, i) => {
+        if (item.homeTeam !== team && item.awayTeam !== team) return;
+        gameNum = gameNum + 1;
+        const game = <div key={item.date} className="game-single">
+                        <span className="game-num">{gameNum}</span>
+                        <div className="game-date">{item.date}</div>
+                        <div className="game-teams">
+                          <div className="game-home-team">{item.homeTeam}</div>
+                          <div className="game-away-team">{item.awayTeam}</div>
+                        </div>
+                        <div className="game-goals">
+                          <div className="game-home-team-goals">{item.fullTimeHomeTeamGoals}</div>
+                          <div className="game-away-team-goals">{item.fullTimeAwayTeamGoals}</div>
+                        </div>
+                      </div>
+        gamesList.push(game);
+      });
+      this.setState({gamesList: gamesList});
+    })
+    .catch((error) => console.log(error));
+  }
+
+  async componentDidMount() {
+    await this.saveLinkPropsToState(this.props.match.params);
+    this.ajaxCallGames (this.state.season, this.state.country, this.state.league, this.state.team);
+  }
+
   render() {
-    const season = this.props.match.params.season;
-    const country = this.props.match.params.country;
-    const league = this.props.match.params.league;
-    const team = this.props.match.params.team;
     return (
       <div>
-        <h3>Team component</h3>
-        <h3>{season}</h3>
-        <h3>{country}</h3>
-        <h3>{league}</h3>
-        <h3>{team}</h3>
+        <Link to="/">Home</Link>
+        <h3 className="team-name">{this.state.team}</h3>
+        <div className="control-statistics">
+          <div className="games-list active">Games list</div>
+          <div className="summary">Summary</div>
+        </div>
+        <div className="games-list">
+          {this.state.gamesList}
+        </div>
       </div>
     );
   }
